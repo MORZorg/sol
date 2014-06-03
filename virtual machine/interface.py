7e976fe7dc503b3521f97779fd5cd36eb9442976
Maurizio Zucchelli <mauzuc90@yahoo.it>
2014-06-03 18:21:32 +0200

    Basic interface working for atomic types

--- virtual machine/interface.py
+++ virtual machine/interface.py
@@ -26,18 +26,61 @@ class InputDialog(QtGui.QDialog):
     def __init__(self, schema):
         QtGui.QDialog.__init__(self)
         self.ui = uic.loadUi("InputDialog.ui", self)
-        self.ui.lineEdit.setText(self.doSomething(schema.split(" ")))
 
-    def doSomething(self, array):
+        #  Remove, Create, Replace
+        self.ui.gridLayout.removeWidget(self.ui.widgetSchema)
+        self.ui.widgetSchema.close()
+        self.ui.widgetSchema = InputDialog.resolveSchema(schema.split(" "))
+        self.ui.gridLayout.addWidget(self.ui.widgetSchema, 0, 0, 1, 1)
+        self.ui.gridLayout.update()
+
+    @staticmethod
+    def resolveSchema(array):
         if len(array) == 0:
             return ""
 
         if array[0] == "INT":
-            text =  "blob"
+            return IntegerWidget()
+        elif array[0] == "REAL":
+            return RealWidget()
+        elif array[0] == "CHAR":
+            return CharacterWidget()
+        elif array[0] == "STRING":
+            return StringWidget()
+        elif array[0] == "BOOL":
+            return BooleanWidget()
         else:
-            text =  "bio"
+            return ""
 
-        return text + self.doSomething(array[1:])
+class IntegerWidget(QtGui.QWidget):
+
+    def __init__(self):
+        QtGui.QWidget.__init__(self)
+        self.ui = uic.loadUi("IntegerWidget.ui", self)
+
+class RealWidget(QtGui.QWidget):
+
+    def __init__(self):
+        QtGui.QWidget.__init__(self)
+        self.ui = uic.loadUi("RealWidget.ui", self)
+
+class CharacterWidget(QtGui.QWidget):
+
+    def __init__(self):
+        QtGui.QWidget.__init__(self)
+        self.ui = uic.loadUi("CharacterWidget.ui", self)
+
+class StringWidget(QtGui.QWidget):
+
+    def __init__(self):
+        QtGui.QWidget.__init__(self)
+        self.ui = uic.loadUi("StringWidget.ui", self)
+
+class BooleanWidget(QtGui.QWidget):
+
+    def __init__(self):
+        QtGui.QWidget.__init__(self)
+        self.ui = uic.loadUi("BooleanWidget.ui", self)
 
 
 app = QtGui.QApplication(sys.argv)
