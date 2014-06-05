17d225a9dc16a1b2625023710e43bc261688b697
Maurizio Zucchelli <mauzuc90@yahoo.it>
2014-06-05 09:58:33 +0200

    PEP8 Fixes

--- virtual machine/interface.py
+++ virtual machine/interface.py
@@ -6,13 +6,22 @@ from collections import deque
 
 from PyQt4 import QtCore, QtGui, uic
 
+
 class MainWindow(QtGui.QMainWindow):
 
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
         self.ui = uic.loadUi("MainWindow.ui", self)
-        self.connect(self.ui.actionOpen, QtCore.SIGNAL('triggered()'), self, QtCore.SLOT('open()'))
-        self.connect(self.ui.pushButton, QtCore.SIGNAL('clicked()'), self, QtCore.SLOT('requestInput()'))
+
+        self.connect(self.ui.actionOpen,
+                     QtCore.SIGNAL('triggered()'),
+                     self,
+                     QtCore.SLOT('open()'))
+
+        self.connect(self.ui.pushButton,
+                     QtCore.SIGNAL('clicked()'),
+                     self,
+                     QtCore.SLOT('requestInput()'))
 
     @QtCore.pyqtSlot()
     def open(self):
@@ -23,6 +32,7 @@ class MainWindow(QtGui.QMainWindow):
     def requestInput(self):
         inputDialog = InputDialog(deque(str(self.ui.lineEdit.text())))
         inputDialog.show()
+
 
 class InputDialog(QtGui.QDialog):
 
@@ -72,7 +82,7 @@ class InputDialog(QtGui.QDialog):
             character = schema.popleft()
 
         return result
-        
+
 
 class IntegerWidget(QtGui.QWidget):
 
@@ -80,11 +90,13 @@ class IntegerWidget(QtGui.QWidget):
         QtGui.QWidget.__init__(self)
         self.ui = uic.loadUi("IntegerWidget.ui", self)
 
+
 class RealWidget(QtGui.QWidget):
 
     def __init__(self):
         QtGui.QWidget.__init__(self)
         self.ui = uic.loadUi("RealWidget.ui", self)
+
 
 class CharacterWidget(QtGui.QWidget):
 
@@ -92,17 +104,20 @@ class CharacterWidget(QtGui.QWidget):
         QtGui.QWidget.__init__(self)
         self.ui = uic.loadUi("CharacterWidget.ui", self)
 
+
 class StringWidget(QtGui.QWidget):
 
     def __init__(self):
         QtGui.QWidget.__init__(self)
         self.ui = uic.loadUi("StringWidget.ui", self)
 
+
 class BooleanWidget(QtGui.QWidget):
 
     def __init__(self):
         QtGui.QWidget.__init__(self)
         self.ui = uic.loadUi("BooleanWidget.ui", self)
+
 
 class VectorWidget(QtGui.QWidget):
 
@@ -114,15 +129,21 @@ class VectorWidget(QtGui.QWidget):
         self.ui.widgets = []
         self.ui.horizontal = (math.floor(nesting) % 2 == 0)
         for i in range(self.size-1):
-            self.ui.widgets.append(InputDialog.resolveSchema(deque(schema), nesting+1))
-            self.ui.gridLayout.addWidget(self.ui.widgets[i], i if self.ui.horizontal else 0, 0 if self.ui.horizontal else i)
+            self.ui.widgets.append(
+                InputDialog.resolveSchema(deque(schema), nesting+1))
+            self.ui.gridLayout.addWidget(self.ui.widgets[i],
+                                         i if self.ui.horizontal else 0,
+                                         0 if self.ui.horizontal else i)
 
         # Awful: last time, no copy
         self.ui.widgets.append(InputDialog.resolveSchema(schema, nesting+1))
-        self.ui.gridLayout.addWidget(self.ui.widgets[self.size-1], self.size-1 if self.ui.horizontal else 0, 0 if self.ui.horizontal else self.size-1)
+        self.ui.gridLayout.addWidget(self.ui.widgets[self.size-1],
+                                     self.size-1 if self.ui.horizontal else 0,
+                                     0 if self.ui.horizontal else self.size-1)
         self.ui.gridLayout.update()
 
-        schema.popleft() # Closed square bracket ending the vector's schema
+        schema.popleft()  # Closed square bracket ending the vector's schema
+
 
 class StructWidget(QtGui.QWidget):
 
@@ -145,16 +166,21 @@ class StructWidget(QtGui.QWidget):
             self.ui.gridLayout.addWidget(self.ui.widgets[i][0], i, 0)
             self.ui.gridLayout.addWidget(self.ui.widgets[i][1], i, 1)
 
-            character = schema.popleft() # Comma separating elements of the struct or closed bracket
+            # Comma separating elements of the struct or closed bracket
+            character = schema.popleft()
             i += 1
         self.ui.gridLayout.update()
+
 
 class NestedWidget(QtGui.QWidget):
 
     def __init__(self, schema):
         QtGui.QWidget.__init__(self)
         self.ui = uic.loadUi("NestedWidget.ui", self)
-        self.connect(self.ui.pushButton, QtCore.SIGNAL('clicked()'), self, QtCore.SLOT('requestInput()'))
+        self.connect(self.ui.pushButton,
+                     QtCore.SIGNAL('clicked()'),
+                     self,
+                     QtCore.SLOT('requestInput()'))
         self.inputDialog = InputDialog(schema)
 
     @QtCore.pyqtSlot()
