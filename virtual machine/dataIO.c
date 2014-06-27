6278ae528db331858a52a2d4601cffd7614deba1
Merry38 <rose.tia38@gmail.com>
2014-06-27 18:31:56 +0200

    Added modified example to test pyqt embedding

--- virtual machine/dataIO.c
+++ virtual machine/dataIO.c
@@ -23,53 +23,77 @@ void finalize_gui(void)
 
 ByteArray userInput(char* schema)
 {
+	fprintf( stderr, "----------------STILL WORKING\n" );
 	PyObject* gui_function;
+	fprintf( stderr, "----------------STILL WORKING\n" );
 	PyObject* gui_args;
+	fprintf( stderr, "----------------STILL WORKING\n" );
 	ByteArray result;
+	fprintf( stderr, "----------------STILL WORKING %d\n", !gui_initialized );
 
 	if( !gui_initialized )
 	{
 		initialize_gui();
+		fprintf( stderr, "----------------STILL WORKING\n" );
 		gui_initialized = GUI_SELF_INIT;
+		fprintf( stderr, "----------------STILL WORKING\n" );
 	}
+	
+	fprintf( stderr, "----------------STILL WORKING\n" );
 
 	gui_function = PyObject_GetAttrString(gui_module,
 										  PYTHON_REQUEST_INPUT_NAME);
+	fprintf( stderr, "----------------STILL WORKING\n" );
 	gui_args = PyTuple_Pack(1, PyUnicode_FromString(schema));
+	fprintf( stderr, "----------------STILL WORKING %d %d %s\n", gui_function == NULL, gui_args == NULL, schema );
+
+	PyObject* a = PyObject_CallObject( gui_function, gui_args );
+	fprintf( stderr, "----------------STILL WORKING\n" );
 
 	PyBytes_AsStringAndSize(
-		PyObject_CallObject(gui_function, gui_args),
+		a,
 		&result.value,
 		&result.size);
+	fprintf( stderr, "----------------STILL WORKING %d\n", gui_initialized == GUI_SELF_INIT );
 
 	if( gui_initialized == GUI_SELF_INIT )
 		finalize_gui();
+	fprintf( stderr, "----------------STILL WORKING\n" );
 
 	return result;
 }
 
 void userOutput(char* schema, ByteArray data)
 {
+	fprintf( stderr, "----------------STILL WORKING\n" );
 	PyObject* gui_function;
+	fprintf( stderr, "----------------STILL WORKING\n" );
 	PyObject* gui_args;
+	fprintf( stderr, "----------------STILL WORKING %d\n", !gui_initialized );
 
 	if( !gui_initialized )
 	{
 		initialize_gui();
+		fprintf( stderr, "----------------STILL WORKING\n" );
 		gui_initialized = GUI_SELF_INIT;
+		fprintf( stderr, "----------------STILL WORKING\n" );
 	}
 
 	gui_function = PyObject_GetAttrString(gui_module,
 										  PYTHON_REQUEST_OUTPUT_NAME);
+	fprintf( stderr, "----------------STILL WORKING\n" );
 	gui_args =
 		PyTuple_Pack(2,
 					 PyUnicode_FromString(schema),
 					 PyBytes_FromStringAndSize(data.value, data.size));
+	fprintf( stderr, "----------------STILL WORKING\n" );
 
 	PyObject_CallObject(gui_function, gui_args);
+	fprintf( stderr, "----------------STILL WORKING %d\n", gui_initialized == GUI_SELF_INIT );
 
 	if( gui_initialized == GUI_SELF_INIT )
 		finalize_gui();
+	fprintf( stderr, "----------------STILL WORKING\n" );
 }
 
 ByteArray fileInput(char* filename)
