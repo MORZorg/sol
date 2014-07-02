24e7f5f48267daa02b05e4ad25c256a89fa3efa8
BoreaWolf <ricky.orizio@gmail.com>
2014-07-02 11:22:22 +0200

    Test 8 works.
    Not at all, number of permutation is not displayed correctly.

--- tests/test8.sol
+++ tests/test8.sol
@@ -7,6 +7,16 @@ func main() : int
 	const	MAX_LEN: int = 15;
 			INTRO: string = "Insert a word:";
 
+	func new_pseudo_string() : pseudo_string
+		var result: pseudo_string;
+			i: int;
+	begin new_pseudo_string
+		for i = 0 to MAX_LEN do
+			result[ i ] = '\0';
+		endfor;
+		return result;
+	end new_pseudo_string
+
 	func strlen( str: pseudo_string; ) : int
 		var i: int;
 	begin strlen
@@ -32,46 +42,61 @@ func main() : int
 		return str1;
 	end strcat
 
-	func permutation( str: pseudo_string; index: int; base: pseudo_string; ) : pseudo_string
-		var result, next_step: pseudo_string;
-			i, j, next_i: int;
+	func strcpy( str: pseudo_string; ) : pseudo_string
+		var result: pseudo_string;
+			i: int;
+	begin strcpy
+		for i = 0 to strlen( str ) do
+			result[ i ] = str[ i ];
+		endfor;
+		return result;
+	end strcpy
+
+	func permutation( to_process: pseudo_string; base: pseudo_string; number: int; ) : pseudo_string
+		var next_step, current_base, result: pseudo_string;
+			i, j: int;
 	begin permutation
 		-- Returning if i have only one char to process
-		if index == 1 then 
-			--return str;
-			write vector( struct( "Permutation:", strcat( base, str ) ) );
-			return strcat( base, str );
+		if strlen( to_process ) == 1 then 
+			write vector( struct( "Permutation:", number + 1, strcat( base, to_process ) ) );
+			return strcat( base, to_process );
 		endif;
 
+		current_base = strcpy( base );
 		j = 0;
-		while j < index do
-		--	-- Resetting the result
-		--	for i = 0 to MAX_LEN do
-		--		result[ i ] = '\0';
-		--		next_step[ i ] = '\0';
-		--	endfor;
+		while j < strlen( to_process ) do
 
+			-- Resetting base to the argument value and resetting next_step
+			base = strcpy( current_base );
+			next_step = new_pseudo_string();
 			-- Creating the new string to pass as argument
-			for i = 0 to index do
+			for i = 0 to strlen( to_process ) do
 				if i != j then
-					next_step = strcat( next_step, str[ i ] );
+					next_step = strcat( next_step, to_process[ i ] );
 				else
-					base = strcat( base, str[ i ] );
+					base = strcat( base, to_process[ i ] );
 				endif;
 			endfor;
 
+		--	write vector(	struct( "Number before", number ),
+		--					struct( "Number after", number + strlen( next_step ) * j  ),
+		--					struct( "J", j ),
+		--					struct( "to_process", strlen( next_step ) ) );
+		--	write vector( struct( "Base", base ), struct( "Next step", next_step ) );
+			number = number + j * strlen( next_step );
 			-- write vector( struct( "Saving", result ), struct( "Next:", next_step ) );
 
 			-- Decrementing the size of the permutations
 			-- result = strcat( base, permutation( next_step, index - 1, base ) );
-			write vector( struct( "Out:", permutation( next_step, index - 1, base ) ) );
+			-- write vector( struct( "Out:", permutation( next_step, base ) ) );
+			result = permutation( next_step, base, number );
 
 			j = j + 1;
-			--write vector( struct( "Permutation:", strcat( base, result ) ), struct( "Base:", base ), struct( "Result:", result ) );
+			-- write vector( struct( "Permutation:", strcat( base, result ) ), struct( "Base:", base ), struct( "Result:", result ) );
 
 		endwhile;
 
-		-- Completly wrong
+		-- Completly useless
 		return result;
 
 	end permutation
@@ -82,11 +107,7 @@ begin main
 
 	write vector( struct( "Word:", word ) );
 
-	for i = 0 to MAX_LEN do
-		empty[ i ] = '\0';
-	endfor;
-
-	word = permutation( word, strlen( word ), empty );
+	word = permutation( word, new_pseudo_string(), 0 );
 
 	return 0;
 
